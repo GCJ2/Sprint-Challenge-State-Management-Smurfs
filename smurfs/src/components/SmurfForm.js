5cfef3f9127aba1a7c4a83ec6d529d8319f2388b
Greg Johnston <GCJohnston91@gmail.com>
2019-08-16 11:26:21 -0400

    Form wired up for state changes through Context. Working on onSubmit now

--- smurfs/src/components/SmurfForm.js
+++ smurfs/src/components/SmurfForm.js
@@ -7,15 +7,23 @@ const SmurfForm = (props) => {
   const {newSmurf, setNewSmurf, smurfs, setSmurfs} = useContext(SmurfContext)
   console.log('newSmurf', newSmurf);
 
+  const handleChange = e => {
+    setNewSmurf({...newSmurf, [e.target.name]: e.target.value})
+  }
+
+  const ageChange = e => {
+    setNewSmurf({...newSmurf, age: parseInt(e.target.value, 10)})
+  }
+
 return(
   <div>
     <h1>Add a new Smurf!</h1>
     <form>
-      <input type="text" name="name" placeholder="What is their name?" value="" />
+      <input type="text" name="name" placeholder="What is their name?" value={newSmurf.name} onChange={handleChange} />
 
-      <input type="text" name="age" placeholder="How old are they?" value="" />
+      <input type="text" name="age" placeholder="How old are they?" value={newSmurf.age} onChange={ageChange} />
 
-      <input type="text" name="height" placeholder="How tall are they?" value="" />
+      <input type="text" name="height" placeholder="How tall are they?" value={newSmurf.height} onChange={handleChange} />
       <button type="button">Add Smurf!</button>
     </form>
   </div>
