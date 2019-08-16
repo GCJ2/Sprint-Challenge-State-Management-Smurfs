fd989160a20e01a77ce90f1a9e3b5728af5a0192
Greg Johnston <GCJohnston91@gmail.com>
2019-08-16 11:07:54 -0400

    Init

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
+import axios from 'axios'
+import {SmurfContext} from '../SmurfContext'
+
 import "./App.css";
-class App extends Component {
-  render() {
+function App () {
+
+
     return (
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+
       </div>
     );
-  }
+
 }
 
 export default App;
