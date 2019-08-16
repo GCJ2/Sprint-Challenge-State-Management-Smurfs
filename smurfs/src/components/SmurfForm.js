b83ca574099b13c8d2647e3e35f35cb82e8cf8b2
Greg Johnston <GCJohnston91@gmail.com>
2019-08-16 11:30:38 -0400

    axios.post working. Smurfs being added to DB

--- smurfs/src/components/SmurfForm.js
+++ smurfs/src/components/SmurfForm.js
@@ -15,16 +15,34 @@ const SmurfForm = (props) => {
     setNewSmurf({...newSmurf, age: parseInt(e.target.value, 10)})
   }
 
+  const submitSmurf = () => {
+    console.log('newSmurf submit', newSmurf);
+    axios.post('http://localhost:3333/smurfs', newSmurf)
+    .then(res => {
+      console.log(res);
+    })
+    .catch(err => {
+      console.log(err.response.data);
+    })
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log('handleSubmit', newSmurf);
+    submitSmurf();
+    setNewSmurf({name:'', age:'', height:''})
+  }
+
 return(
   <div>
     <h1>Add a new Smurf!</h1>
-    <form>
+    <form onSubmit={handleSubmit}>
       <input type="text" name="name" placeholder="What is their name?" value={newSmurf.name} onChange={handleChange} />
 
       <input type="text" name="age" placeholder="How old are they?" value={newSmurf.age} onChange={ageChange} />
 
       <input type="text" name="height" placeholder="How tall are they?" value={newSmurf.height} onChange={handleChange} />
-      <button type="button">Add Smurf!</button>
+      <button type="button" onClick={handleSubmit}>Add Smurf!</button>
     </form>
   </div>
   )
