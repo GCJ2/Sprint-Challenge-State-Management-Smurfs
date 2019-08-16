1dfed96f4cedd6e3e35ac6d26f3d1b732055e26a
Greg Johnston <GCJohnston91@gmail.com>
2019-08-16 11:22:47 -0400

    Form up and running. Working on wiriing.

--- smurfs/src/components/SmurfForm.js
+++ smurfs/src/components/SmurfForm.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import {SmurfContext} from '../SmurfContext'
+import axios from 'axios'
 
 const SmurfForm = (props) => {
+
+  const {newSmurf, setNewSmurf, smurfs, setSmurfs} = useContext(SmurfContext)
+  console.log('newSmurf', newSmurf);
+
 return(
   <div>
-    SmurfForm
+    <h1>Add a new Smurf!</h1>
+    <form>
+      <input type="text" name="name" placeholder="What is their name?" value="" />
+
+      <input type="text" name="age" placeholder="How old are they?" value="" />
+
+      <input type="text" name="height" placeholder="How tall are they?" value="" />
+      <button type="button">Add Smurf!</button>
+    </form>
   </div>
   )
 }
